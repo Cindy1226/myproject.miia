039e6de85a107c1e8ee9e5867ea921c6cf785943
Cindy1226 <93966287+Cindy1226@users.noreply.github.com>
2022-01-04 16:37:46 +0800

    Add files via upload

--- src/main/java/myproject/Buy.java
+++ src/main/java/myproject/Buy.java
@@ -6,12 +6,41 @@ public class Buy {
     public static void main(String[] args) {
         int n=-1;
         Scanner scanner = new Scanner(System.in);
+        double total = 0;
         while (n!=0){
-            System.out.println("buy item");
-            System.out.println("1) ");
-            System.out.println("2)");
-            System.out.println("3)");
-            System.out.println("0)");
+
+            System.out.println("buy item:");
+            System.out.println("1)Dress ");
+            System.out.println("2)Sox");
+            System.out.println("3)Shoes");
+            System.out.println("0)Exit");
+            String  s =scanner.next();
+            int num=Integer.parseInt(s);
+            switch (s){
+                case "1":
+                    total+=500;
+                    break;
+                case "2":
+                    total+=450;
+                    break;
+                case"3":
+                    total+=350;
+                    break;
+
+
+
+            }
+
+            if(num==0){
+                n++;
+                System.out.println("total:"+total);
+            }
+
+
+        }
+        if (total>1000){
+            System.out.println(total*0.9);
+        }
+
         }
     }
-}
