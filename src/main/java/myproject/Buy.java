2584deacfe3393e6ae2183a9c28f5884da74f174
Cindy1226 <93966287+Cindy1226@users.noreply.github.com>
2022-01-04 15:11:06 +0800

    Add files via upload

---
+++ src/main/java/myproject/Buy.java
@@ -0,0 +1,17 @@
+package com.cindy;
+
+import java.util.Scanner;
+
+public class Buy {
+    public static void main(String[] args) {
+        int n=-1;
+        Scanner scanner = new Scanner(System.in);
+        while (n!=0){
+            System.out.println("buy item");
+            System.out.println("1) ");
+            System.out.println("2)");
+            System.out.println("3)");
+            System.out.println("0)");
+        }
+    }
+}
